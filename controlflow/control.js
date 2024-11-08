a91cad4504987bbcf65dc45d6044af5fb4aa46a8
fathinabdurahman <114554964+fathinabdurahman@users.noreply.github.com>
2024-11-08 10:20:27 +0700

    Add files via upload

---
+++ controlflow/control.js
@@ -0,0 +1,15 @@
+// 1. Our First For Loop Practice
+for (let i = 0; i < 6; i++) {
+    console.log("ifheihg be");
+}
+
+// 2. For Loops Practice
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
+
+// 3. Iterating Arrays Exercise
+const people = ["aku", "kamu", "dia", "kalian", "kita"];
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
